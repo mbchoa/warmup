78bbe138bb7094e068eaabbf23e3e04c878ce846
Michael-Bryant Choa <mbchoa@gmail.com>
2018-11-25 23:35:08 +0900

    Add A2HS prompt logic, update manifest

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import WarmupInput from './WarmupInput';
 
 const App = () => {
+  useEffect(() => {
+    window.addEventListener('beforeinstallprompt', (e) => {
+      e.preventDefault();
+      e.prompt();
+      e.userChoice.then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+          console.log('User accepted the A2HS prompt');
+        } else {
+          console.log('User dismissed the A2HS prompt');
+        }
+      });
+    });
+  })
   return (
     <div className="app">
       <div className="app__content">
