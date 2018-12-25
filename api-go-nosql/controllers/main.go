3e109e20ae8ade6da4918e4bf7beb34762c91a4d
Michael-Bryant Choa <mbchoa@gmail.com>
2018-12-24 20:58:51 -0800

    Implement get all workouts endpoint

--- api-go-nosql/controllers/main.go
+++ api-go-nosql/controllers/main.go
@@ -7,6 +7,7 @@ import (
   "net/http"
   "github.com/gin-gonic/gin"
   "github.com/mongodb/mongo-go-driver/bson/primitive"
+  "github.com/mongodb/mongo-go-driver/mongo/options"
   "github.com/mbchoa/warmup/api-go-nosql/models"
   "github.com/mbchoa/warmup/api-go-nosql/db"
 )
@@ -30,6 +31,7 @@ func CreateWorkout(c *gin.Context) {
     c.JSON(http.StatusCreated, map[string]interface{}{
       "_id": fmt.Sprintf("http://localhost:8080/api/v1/workouts/%s", oid.Hex()),
     })
+    return
   }
 
   c.JSON(http.StatusInternalServerError, gin.H{"error": "Error type asserting InsertedID"})
@@ -37,6 +39,26 @@ func CreateWorkout(c *gin.Context) {
 
 func GetWorkouts(c *gin.Context) {
   fmt.Println("Get all workouts")
+
+  findOptions := options.Find()
+  cur, err := db.Collection.Find(context.TODO(), nil, findOptions)
+  if err != nil {
+    c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+    return
+  }
+  defer cur.Close(context.TODO())
+
+  var data []models.Workout
+  for cur.Next(context.TODO()) {
+    var singleWorkout models.Workout
+    err := cur.Decode(&singleWorkout)
+    if err != nil {
+      // Do something
+    }
+
+    data = append(data, singleWorkout)
+  }
+  c.JSON(http.StatusOK, data)
 }
 
 func GetWorkoutById(c *gin.Context) {
