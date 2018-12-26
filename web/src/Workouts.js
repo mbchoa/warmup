6fe766e781968148adca2cb15f3382a5a7b46eb3
Michael-Bryant Choa <mbchoa@gmail.com>
2018-12-26 00:17:50 -0800

    Implement Workouts component and wireup to API

--- web/src/Workouts.js
+++ web/src/Workouts.js
@@ -1,13 +1,55 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { format } from 'date-fns';
 
-import './styles/_workouts.scss'
+import './styles/_workouts.scss';
 
 const Workouts = () => {
+  const [workouts, setWorkouts] = useState([]);
+
+  function handleWorkoutsJson(workouts) {
+    setWorkouts(workouts)
+  }
+
+  useEffect(() => {
+    fetch('http://localhost:8080/api/v1/workouts/')
+      .then(res => res.json())
+      .then(handleWorkoutsJson)
+  }, [])
+
   return (
     <div className="workouts">
-      Workouts
+      {workouts && workouts.map(({ created_at, exercises }) => (
+        <div className="workouts__instance">
+          <h1 className="workouts__instance-date">
+            {format(created_at, 'MMMM D YYYY')}
+          </h1>
+          <hr />
+          {exercises && exercises.map(({ type, warmup_sets, work_sets }) => (
+            <div className="workouts__exercises">
+              <h2 className="workouts__exercise-type">
+                {type}
+              </h2>
+              <div className="workouts__exercise-sets">
+                {warmup_sets && warmup_sets.map(({ weight, reps }) => (
+                  <div className="workouts__exercise-set">
+                    {reps} x {weight} lbs
+                  </div>
+                ))}
+              </div>
+              <div className="workouts__exercise-sets">
+                {work_sets && work_sets.map(({ weight, reps }) => (
+                  <div className="workouts__exercise-set">
+                    {reps} x {weight} lbs
+                  </div>
+                ))}
+              </div>
+            </div>
+          ))}
+
+        </div>
+      ))}
     </div>
   )
-}
+};
 
 export default Workouts;
