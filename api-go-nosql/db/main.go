ac7b847c4d297aca9f2062a2d7c781948a1a2ff9
Michael-Bryant Choa <mbchoa@gmail.com>
2018-12-25 17:21:00 -0800

    Add Makefile and update app to use environment variables

--- api-go-nosql/db/main.go
+++ api-go-nosql/db/main.go
@@ -4,30 +4,32 @@ import (
   "context"
   "fmt"
   "log"
+  "os"
   "github.com/mongodb/mongo-go-driver/mongo"
 )
 
 var Collection *mongo.Collection
 
-func Init() {
-  // Connect to mongo db
-  client, err := mongo.Connect(context.TODO(), "mongodb://localhost:27017")
+func dbConnectionString() string {
+  return fmt.Sprintf("mongodb://%s:%s", os.Getenv("MONGO_HOST"), os.Getenv("MONGO_PORT"))
+}
 
+func Init() {
+  fmt.Println("Initialize database")
+  // Connect to mongo db
+  client, err := mongo.Connect(context.TODO(), dbConnectionString())
   if err != nil {
     log.Fatal(err)
   }
 
   // Check connection
   err = client.Ping(context.TODO(), nil)
-
   if err != nil {
     log.Fatal(err)
   }
 
-  fmt.Println("Connected to MongoDB!")
-
   // save reference to collection object
-  Collection = client.Database("workout").Collection("exercises")
+  Collection = client.Database(os.Getenv("MONGO_DB")).Collection(os.Getenv("MONGO_COLLECTION"))
 
   fmt.Println("Connected to MongoDB!")
 }
