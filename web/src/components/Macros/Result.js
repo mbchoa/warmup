4f5f2a094bc6ae54f3046d53f5401cdace9093ee
Michael-Bryant Choa <mbchoa@gmail.com>
2019-01-06 22:56:32 -0800

    Style result tab buttons

--- web/src/components/Macros/Result.js
+++ web/src/components/Macros/Result.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import cx from 'classnames';
+import { partial } from 'lodash';
 
 const Result = () => {
+  const [selectedTabIndex, setTabIndex] = useState(1)
+
+  function handleTabClick(tabIndex) {
+    setTabIndex(tabIndex);
+  }
+
   return (
     <div className="result">
       <ul className="result__tabs">
-        <li className="result__tab">Lose Weight</li>
-        <li className="result__tab">Maintain Weight</li>
-        <li className="result__tab">Gain Weight</li>
+        <li className={cx(
+          "result__tab",
+          {"result__tab--selected": selectedTabIndex === 0},
+        )} onClick={partial(handleTabClick, 0)}>Lose Weight</li>
+        <li className={cx(
+          "result__tab",
+          {"result__tab--selected": selectedTabIndex === 1},
+        )} onClick={partial(handleTabClick, 1)}>Maintain Weight</li>
+        <li className={cx(
+          "result__tab",
+          {"result__tab--selected": selectedTabIndex === 2},
+        )} onClick={partial(handleTabClick, 2)}>Gain Weight</li>
       </ul>
       <div className="result__content">
         2245 Calories
