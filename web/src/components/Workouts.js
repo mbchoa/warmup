640280de246b71629a4617ae31fa0c2fe95264b3
Michael-Bryant Choa <mbchoa@gmail.com>
2019-02-03 00:26:08 -0800

    Update styling to be responsive

--- web/src/components/Workouts.js
+++ web/src/components/Workouts.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { format } from 'date-fns';
 
+import Card from './Card';
+
 import '../styles/workouts.scss';
 
 const Workouts = () => {
@@ -21,33 +23,37 @@ const Workouts = () => {
       <h1 className="workouts__header">workouts</h1>
       <div className="workouts__content">
         {workouts && workouts.map(({ created_at, exercises }) => (
-          <div className="workouts__instance">
-            <h1 className="workouts__instance-date">
-              {format(created_at, 'MMMM D, YYYY')}
-            </h1>
-            <hr />
-            {exercises && exercises.map(({ type, warmup_sets, work_sets }) => (
-              <div className="workouts__exercises">
-                <h2 className="workouts__exercise-type">
-                  {type}
-                </h2>
-                <div className="workouts__exercise-sets">
-                  {warmup_sets && warmup_sets.map(({ weight, reps }) => (
-                    <div className="workouts__exercise-set">
-                      {reps} x {weight} lbs
-                    </div>
-                  ))}
+          <div className="workouts__log">
+            <Card>
+              <h5 className="workouts__day-of-week">
+                {format(created_at, 'ddd')}
+              </h5>
+              <h1 className="workouts__date">
+                {format(created_at, 'MMMM Wo')}
+              </h1>
+              <hr />
+              {exercises && exercises.map(({ type, warmup_sets, work_sets }) => (
+                <div className="workouts__exercises">
+                  <h2 className="workouts__exercise-type">
+                    {type}
+                  </h2>
+                  <div className="workouts__exercise-sets">
+                    {warmup_sets && warmup_sets.map(({ weight, reps }) => (
+                      <div className="workouts__exercise-set">
+                        {reps} x {weight} lbs
+                      </div>
+                    ))}
+                  </div>
+                  <div className="workouts__exercise-sets">
+                    {work_sets && work_sets.map(({ weight, reps }) => (
+                      <div className="workouts__exercise-set">
+                        {reps} x {weight} lbs
+                      </div>
+                    ))}
+                  </div>
                 </div>
-                <div className="workouts__exercise-sets">
-                  {work_sets && work_sets.map(({ weight, reps }) => (
-                    <div className="workouts__exercise-set">
-                      {reps} x {weight} lbs
-                    </div>
-                  ))}
-                </div>
-              </div>
-            ))}
-
+              ))}
+            </Card>
           </div>
         ))}
       </div>
