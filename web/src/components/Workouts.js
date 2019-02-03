552022d627437cf18ec7a002c5bce7fab3b78fce
Michael-Bryant Choa <mbchoa@gmail.com>
2019-02-02 23:57:56 -0800

    Add initial workouts styling

--- web/src/components/Workouts.js
+++ web/src/components/Workouts.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { format } from 'date-fns';
 
+import '../styles/workouts.scss';
+
 const Workouts = () => {
   const [workouts, setWorkouts] = useState([]);
 
@@ -16,36 +18,39 @@ const Workouts = () => {
 
   return (
     <div className="workouts">
-      {workouts && workouts.map(({ created_at, exercises }) => (
-        <div className="workouts__instance">
-          <h1 className="workouts__instance-date">
-            {format(created_at, 'MMMM D YYYY')}
-          </h1>
-          <hr />
-          {exercises && exercises.map(({ type, warmup_sets, work_sets }) => (
-            <div className="workouts__exercises">
-              <h2 className="workouts__exercise-type">
-                {type}
-              </h2>
-              <div className="workouts__exercise-sets">
-                {warmup_sets && warmup_sets.map(({ weight, reps }) => (
-                  <div className="workouts__exercise-set">
-                    {reps} x {weight} lbs
-                  </div>
-                ))}
+      <h1 className="workouts__header">workouts</h1>
+      <div className="workouts__content">
+        {workouts && workouts.map(({ created_at, exercises }) => (
+          <div className="workouts__instance">
+            <h1 className="workouts__instance-date">
+              {format(created_at, 'MMMM D, YYYY')}
+            </h1>
+            <hr />
+            {exercises && exercises.map(({ type, warmup_sets, work_sets }) => (
+              <div className="workouts__exercises">
+                <h2 className="workouts__exercise-type">
+                  {type}
+                </h2>
+                <div className="workouts__exercise-sets">
+                  {warmup_sets && warmup_sets.map(({ weight, reps }) => (
+                    <div className="workouts__exercise-set">
+                      {reps} x {weight} lbs
+                    </div>
+                  ))}
+                </div>
+                <div className="workouts__exercise-sets">
+                  {work_sets && work_sets.map(({ weight, reps }) => (
+                    <div className="workouts__exercise-set">
+                      {reps} x {weight} lbs
+                    </div>
+                  ))}
+                </div>
               </div>
-              <div className="workouts__exercise-sets">
-                {work_sets && work_sets.map(({ weight, reps }) => (
-                  <div className="workouts__exercise-set">
-                    {reps} x {weight} lbs
-                  </div>
-                ))}
-              </div>
-            </div>
-          ))}
+            ))}
 
-        </div>
-      ))}
+          </div>
+        ))}
+      </div>
     </div>
   )
 };
